6eb54eac500008a4d1f1ce70900218cd74083ff5
DanielVandH <danj.vandenheuvel@gmail.com>
2022-12-04 20:40:35 +1000

    Travelling wave tests

--- README.md
+++ README.md
@@ -11,6 +11,7 @@
   - [Diffusion equation in a wedge with mixed boundary conditions](#diffusion-equation-in-a-wedge-with-mixed-boundary-conditions)
   - [Reaction-diffusion equation with a time-dependent Dirichlet boundary condition on a disk](#reaction-diffusion-equation-with-a-time-dependent-dirichlet-boundary-condition-on-a-disk)
   - [Porous medium equation](#porous-medium-equation)
+  - [Porous-Fisher equatioon and travelling waves](#porous-fisher-equatioon-and-travelling-waves)
 
 This is a package for solving partial differential equations (PDEs) of the form 
 
@@ -94,7 +95,7 @@ is_neumann_type(type) = type ∈ (:Neumann, :N, :neumann, "Neumann", "N", "neuma
 is_dudt_type(type) = type ∈ (:Dudt, :dudt, "Dudt", "dudt", "du/dt")
 ```
 
-For example, `types = (:dudt, :neumann, :D, :D)` means that the first segment has a time-dependent Dirichlet boundary condition, the second a homogeneous Neumann boundary condition, and the last two segments have Dirichlet boundary conditions (with possibly different functions). The argument `boundary_node_vector` is the same as `BNV`. To provide the parameters `p` for each function, the keyword argument `params` is provided, letting `params[i]` be the set of parameters used when calling `functions[i]`. The type of the solution `u` can be declared using `u_type`, and the numbers representing the coordinates can be declared using `float_type`.
+For example, `types = (:dudt, :neumann, :D, :D)` means that the first segment has a time-dependent Dirichlet boundary condition, the second a homogeneous Neumann boundary condition, and the last two segments have Dirichlet boundary conditions (with possibly different functions). The argument `boundary_node_vector` is the same as `BNV`. To provide the parameters `p` for each function, the keyword argument `params` is provided, letting `params[i]` be the set of parameters used when calling `functions[i]`. The type of the solution `u` can be declared using `u_type`, and the numbers representing the coordinates can be declared using `float_type`. Note that the values for any functions corresponding to a Neumann boundary condition are currently ignored (equivalent to assuming the function is zero).
 
 ## The problem 
 
@@ -178,6 +179,7 @@ using OrdinaryDiffEq
 using LinearSolve 
 using CairoMakie 
 using Bessels
+using Test
 ```
 
 ## Diffusion equation on a square plate 
@@ -277,7 +279,6 @@ u: 11-element Vector{Vector{Float64}}:
 
 You can use `sol` as you would any other solution from `DifferentialEquations.jl` (e.g. `sol(t)` returns the solution at time `t`). To visualise the solution at the times `t = 0.0`, `t = 0.25`, and `t = 0.5`, the following code can be used:
 ```julia
-using CairoMakie 
 pt_mat = Matrix(points')
 T_mat = [collect(T)[i][j] for i in 1:length(T), j in 1:3]
 fig = Figure(resolution=(2068.72f0, 686.64f0), fontsize=38)
@@ -577,3 +578,115 @@ alg = TRBDF2(linsolve=KLUFactorization())
 sol = solve(prob, alg; saveat=2.5)
 ```
 ![Porous-medium equation with linear source](https://github.com/DanielVandH/FiniteVolumeMethod.jl/blob/main/figures/porous_medium_linear_source_test.png?raw=true)
+
+## Porous-Fisher equatioon and travelling waves 
+
+We now consider a more involved example, where we discuss the travelling wave solutions of the Porous-Fisher equation and discuss how we test a more complicated problem. We consider:
+
+$$
+\begin{equation*}
+\begin{array}{rcll}
+\dfrac{\partial u(x, y, t)}{\partial t} &=& D\boldsymbol{\nabla} \boldsymbol{\cdot} [u \boldsymbol{\nabla} u] + \lambda u(1-u), & 0 < x < a, 0 < y < b, t > 0, \\
+u(x, 0, t) & = & 1 & 0 < x < a, t > 0, \\
+u(x, b, t) & = & 0 & 0 < x < a , t > 0, \\
+\dfrac{\partial u(0, y, t)}{\partial x} & = & 0 & 0 < y < b, t > 0, \\
+\dfrac{\partial u(a, y, t)}{\partial x} & = & 0 & 0 < y < b, t > 0, \\
+u(x, y, 0) & = & f(y) & 0 \leq x \leq a, 0 \leq y \leq b. 
+\end{array}
+\end{equation*}
+$$
+
+This problem is defined on the rectangle $[0, a] \times [0, b]$, and we assume that $b \gg a$ so that the rectangle is much taller than it is wide. This problem has $u$ ranging from $u=1$ at the bottom of the rectangle down to $u=0$ at the top of the rectangle, with no flux conditions on the two vertical walls. The problem $f(y)$ is taken to be independent of $x$. This setup implies that the solution along each constant line $x = x_0$ should be about the same, i.e. the problem is invariant in $x$. If indeed we have $u(x, y, t) \equiv u(y, t)$, then the PDE becomes
+
+$$
+\dfrac{\partial u(y, t)}{\partial t} = D\dfrac{\partial}{\partial y}\left(u\dfrac{\partial u}{\partial y}\right) + \lambda u(1-u),
+$$
+
+which has travelling wave solutions. Following the analysis from Section 13.4 of *Mathematical biology I: An introduction* by J. D. Murray (2002), we can show that a travelling wave solution to the one-dimensional problem above is
+
+$$
+u(y, t) = \begin{cases} 1 - \mathrm{e}^{c_{\min}z} & z \leq 0, \\ 0 & z > 0, \end{cases}
+$$
+
+where $c_{\min} = \sqrt{\lambda/(2D)}$, $c = \sqrt{D\lambda/2}$, and $z = x - ct$ is the travelling wave coordinate. This travelling wave would match our problem exactly if the rectangle were instead $[0, a] \times \mathbb R$, but by choosing $b$ large enough we can at least emulate the travelling wave behaviour closely; homogeneous Neumann conditions are to ensure no energy is lost, thus allowing the travelling waves to exist. Note also that the approximations of the solution with $u(y, t)$ above will only be accurate for large time.
+
+Let us now solve the problem. For this problem, rather than using `generate_mesh` we will use a structured triangulation with `triangulate_structured`. This will make it easier to test the $x$ invariance.
+
+```julia
+## Step 1: Define the mesh 
+a, b, c, d, Nx, Ny = 0.0, 3.0, 0.0, 40.0, 60, 80
+T, adj, adj2v, DG, points, BN = triangulate_structured(a, b, c, d, Nx, Ny; return_boundary_types=true)
+mesh = FVMGeometry(T, adj, adj2v, DG, points, BN)
+
+## Step 2: Define the boundary conditions 
+a₁ = ((x, y, t, u::T, p) where {T}) -> one(T)
+a₂ = ((x, y, t, u::T, p) where {T}) -> zero(T)
+a₃ = ((x, y, t, u::T, p) where {T}) -> zero(T)
+a₄ = ((x, y, t, u::T, p) where {T}) -> zero(T)
+bc_fncs = (a₁, a₂, a₃, a₄)
+types = (:D, :N, :D, :N)
+BCs = BoundaryConditions(mesh, bc_fncs, types, BN)
+
+## Step 3: Define the actual PDE  
+f = ((x::T, y::T) where {T}) -> zero(T)
+diff_fnc = (x, y, t, u, p) -> p * u
+reac_fnc = (x, y, t, u, p) -> p * u * (1 - u)
+D, λ = 0.9, 0.99
+diff_parameters = D
+reac_parameters = λ
+final_time = 50.0
+u₀ = [f(points[:, i]...) for i in axes(points, 2)]
+prob = FVMProblem(mesh, BCs; diffusion_function=diff_fnc, reaction_function=reac_fnc,
+    diffusion_parameters=diff_parameters, reaction_parameters=reac_parameters,
+    initial_condition=u₀, final_time)
+
+## Step 4: Solve
+alg = TRBDF2(linsolve=KLUFactorization())
+sol = solve(prob, alg; saveat=0.5)
+```
+
+This gives us our solution. To verify the $x$-invariance, something like the following suffices:
+```julia
+u_mat = [reshape(u, (Nx, Ny)) for u in sol.u]
+all_errs = zeros(length(sol))
+err_cache = zeros((Nx - 1) * Ny)
+for i in eachindex(sol)
+    u = u_mat[i]
+    ctr = 1
+    for j in union(1:((Nx÷2)-1), ((Nx÷2)+1):Nx)
+        for k in 1:Ny
+            err_cache[ctr] = 100abs(u[j, k] .- u[Nx÷2, k])
+            ctr += 1
+        end
+    end
+    all_errs[i] = mean(err_cache)
+end
+@test all(all_errs .< 0.05)
+```
+In this code, we test the $x$-invariance by seeing if the $u(x, y) \approx u(x_0, y)$ for each $x$, where $x_0$ is the midpoint $a/2$.
+
+To now see the travelling wave behaviour, we use the following:
+```julia 
+large_time_idx = findfirst(sol.t .== 10)
+c = sqrt(λ / (2D))
+cₘᵢₙ = sqrt(λ * D / 2)
+zᶜ = 0.0
+exact_solution = ((z::T) where {T}) -> ifelse(z ≤ zᶜ, 1 - exp(cₘᵢₙ * (z - zᶜ)), zero(T))
+travelling_wave_values = zeros(Ny, length(sol) - large_time_idx + 1)
+z_vals = zeros(Ny, length(sol) - large_time_idx + 1)
+for (i, t_idx) in pairs(large_time_idx:lastindex(sol))
+    u = u_mat[t_idx]
+    τ = sol.t[t_idx]
+    for k in 1:Ny
+        y = c + (k - 1) * (d - c) / (Ny - 1)
+        z = y - c * τ
+        z_vals[k, i] = z
+        travelling_wave_values[k, i] = u[Nx÷2, k]
+    end
+end
+exact_z_vals = collect(LinRange(extrema(z_vals)..., 500))
+exact_travelling_wave_values = exact_solution.(exact_z_vals)
+```
+The results we obtain are shown below,with the exact travelling wave from the one-dimensional problem shown in red in the fourth plot and the numerical solutions are the other curves.
+
+![Travelling wave problem](https://github.com/DanielVandH/FiniteVolumeMethod.jl/blob/main/figures/travelling_wave_problem_test.png?raw=true)
--- test/new_tests.jl
+++ test/new_tests.jl
@@ -1576,117 +1576,230 @@ end
 ## Example V: The Porous-Medium equation with a linear source
 ##
 ###########################################################
-## Step 0: Define all the parameters 
-m = 3.4
-M = 2.3
-D = 0.581
-λ = 0.2
-final_time = 10.0
-ε = 0.1
+@testset "Porous-Medium equation with a linear source" begin
+    ## Step 0: Define all the parameters 
+    m = 3.4
+    M = 2.3
+    D = 0.581
+    λ = 0.2
+    final_time = 10.0
+    ε = 0.1
 
+    ## Step 1: Define the mesh 
+    RmM = 4m / (m - 1) * (M / (4π))^((m - 1) / m)
+    L = sqrt(RmM) * (D / (λ * (m - 1)) * (exp(λ * (m - 1) * final_time) - 1))^(1 / (2m))
+    n = 500
+    x₁ = LinRange(-L, L, n)
+    x₂ = LinRange(L, L, n)
+    x₃ = LinRange(L, -L, n)
+    x₄ = LinRange(-L, -L, n)
+    y₁ = LinRange(-L, -L, n)
+    y₂ = LinRange(-L, L, n)
+    y₃ = LinRange(L, L, n)
+    y₄ = LinRange(L, -L, n)
+    x = reduce(vcat, [x₁, x₂, x₃, x₄])
+    y = reduce(vcat, [y₁, y₂, y₃, y₄])
+    xy = [(x, y) for (x, y) in zip(x, y)]
+    unique!(xy)
+    x = getx.(xy)
+    y = gety.(xy)
+    r = 0.07
+    T, adj, adj2v, DG, points, BN = generate_mesh(x, y, r; gmsh_path=GMSH_PATH)
+    mesh = FVMGeometry(T, adj, adj2v, DG, points, BN)
+
+    ## Step 2: Define the boundary conditions 
+    bc = ((x, y, t, u::T, p) where {T}) -> zero(T)
+    types = :D
+    BCs = BoundaryConditions(mesh, bc, types, BN)
+
+    ## Step 3: Define the exact solution for comparison later 
+    function porous_medium_exact_solution(x, y, t, m, M)
+        u_exact = zeros(length(x))
+        RmM = 4m / (m - 1) * (M / (4π))^((m - 1) / m)
+        for i in eachindex(x)
+            if x[i]^2 + y[i]^2 < RmM * t^(1 / m)
+                u_exact[i] = t^(-1 / m) * ((M / (4π))^((m - 1) / m) - (m - 1) / (4m) * (x[i]^2 + y[i]^2) * t^(-1 / m))^(1 / (m - 1))
+            else
+                u_exact[i] = 0.0
+            end
+        end
+        return u_exact
+    end
+    function porous_medium_linear_source_exact_solution(x, y, t, m, M, D, λ)
+        return exp(λ * t) * porous_medium_exact_solution(x, y, D / (λ * (m - 1)) * (exp(λ * (m - 1) * t) - 1), m, M)
+    end
+
+    ## Step 4: Define the actual PDE  
+    f = (x, y) -> M * 1 / (ε^2 * π) * exp(-1 / (ε^2) * (x^2 + y^2))
+    diff_fnc = (x, y, t, u, p) -> p[1] * abs(u)^(p[2] - 1)
+    reac_fnc = (x, y, t, u, p) -> p[1] * u
+    diff_parameters = (D, m)
+    react_parameter = λ
+    u₀ = [f(points[:, i]...) for i in axes(points, 2)]
+    prob = FVMProblem(mesh, BCs; diffusion_function=diff_fnc,
+        diffusion_parameters=diff_parameters,
+        reaction_function=reac_fnc, reaction_parameters=react_parameter,
+        initial_condition=u₀, final_time)
+
+    ## Step 5: Solve
+    alg = TRBDF2(linsolve=KLUFactorization())
+    sol = solve(prob, alg; saveat=2.5)
+
+    ## Step 6: Visualisation 
+    pt_mat = Matrix(points')
+    T_mat = [collect(T)[i][j] for i in 1:length(T), j in 1:3]
+    fig = Figure(resolution=(2131.8438f0, 684.27f0), fontsize=38)
+    ax = Axis(fig[1, 1], width=600, height=600)
+    mesh!(ax, pt_mat, T_mat, color=sol.u[1], colorrange=(0.0, 0.5), colormap=:matter)
+    ax = Axis(fig[1, 2], width=600, height=600)
+    mesh!(ax, pt_mat, T_mat, color=sol.u[3], colorrange=(0.0, 0.5), colormap=:matter)
+    ax = Axis(fig[1, 3], width=600, height=600)
+    mesh!(ax, pt_mat, T_mat, color=sol.u[5], colorrange=(0.0, 0.5), colormap=:matter)
+    SAVE_FIGURE && save("figures/porous_medium_linear_source_test.png", fig)
+
+    ## Step 7: Compare the results
+    all_errs = [Float64[] for _ in eachindex(sol)]
+    u_exact = [porous_medium_linear_source_exact_solution(points[1, :], points[2, :], τ, m, M, D, λ) for τ in sol.t]
+    u_fvm = reduce(hcat, sol.u)
+    u_exact = reduce(hcat, u_exact)
+    errs = reduce(hcat, [100abs.(u - û) / maximum(abs.(u)) for (u, û) in zip(eachcol(u_exact[:, 2:end]), eachcol(u_fvm[:, 2:end]))])
+    @test all(<(0.26), mean.(eachcol(errs)))
+    @test all(<(0.17), median.(eachcol(errs)))
+    @test mean(errs) < 0.25
+    @test median(errs) < 0.05
+
+    ## Step 8: Visualise the comparison 
+    fig = Figure(fontsize=42, resolution=(3586.6597f0, 1466.396f0))
+    ax = Axis(fig[1, 1], width=600, height=600, title=L"(a):$ $ Exact solution, $t = %$(sol.t[1])$", titlealign=:left)
+    mesh!(ax, pt_mat, T_mat, color=u_exact[:, 1], colorrange=(0, 0.05), colormap=:matter)
+    ax = Axis(fig[1, 2], width=600, height=600, title=L"(b):$ $ Exact solution, $t = %$(sol.t[2])$", titlealign=:left)
+    mesh!(ax, pt_mat, T_mat, color=u_exact[:, 2], colorrange=(0, 0.05), colormap=:matter)
+    ax = Axis(fig[1, 3], width=600, height=600, title=L"(c):$ $ Exact solution, $t = %$(sol.t[3])$", titlealign=:left)
+    mesh!(ax, pt_mat, T_mat, color=u_exact[:, 3], colorrange=(0, 0.05), colormap=:matter)
+    ax = Axis(fig[1, 4], width=600, height=600, title=L"(d):$ $ Exact solution, $t = %$(sol.t[4])$", titlealign=:left)
+    mesh!(ax, pt_mat, T_mat, color=u_exact[:, 4], colorrange=(0, 0.05), colormap=:matter)
+    ax = Axis(fig[1, 5], width=600, height=600, title=L"(e):$ $ Exact solution, $t = %$(sol.t[5])$", titlealign=:left)
+    mesh!(ax, pt_mat, T_mat, color=u_exact[:, 5], colorrange=(0, 0.05), colormap=:matter)
+    ax = Axis(fig[2, 1], width=600, height=600, title=L"(f):$ $ Numerical solution, $t = %$(sol.t[1])$", titlealign=:left)
+    mesh!(ax, pt_mat, T_mat, color=u_fvm[:, 1], colorrange=(0, 0.05), colormap=:matter)
+    ax = Axis(fig[2, 2], width=600, height=600, title=L"(g):$ $ Numerical solution, $t = %$(sol.t[2])$", titlealign=:left)
+    mesh!(ax, pt_mat, T_mat, color=u_fvm[:, 2], colorrange=(0, 0.05), colormap=:matter)
+    ax = Axis(fig[2, 3], width=600, height=600, title=L"(h):$ $ Numerical solution, $t = %$(sol.t[3])$", titlealign=:left)
+    mesh!(ax, pt_mat, T_mat, color=u_fvm[:, 3], colorrange=(0, 0.05), colormap=:matter)
+    ax = Axis(fig[2, 4], width=600, height=600, title=L"(i):$ $ Numerical solution, $t = %$(sol.t[4])$", titlealign=:left)
+    mesh!(ax, pt_mat, T_mat, color=u_fvm[:, 4], colorrange=(0, 0.05), colormap=:matter)
+    ax = Axis(fig[2, 5], width=600, height=600, title=L"(j):$ $ Numerical solution, $t = %$(sol.t[5])$", titlealign=:left)
+    mesh!(ax, pt_mat, T_mat, color=u_fvm[:, 5], colorrange=(0, 0.05), colormap=:matter)
+    SAVE_FIGURE && save("figures/porous_medium_linear_source_test_error.png", fig)
+end
+
+###########################################################
+##
+## Example VI: Travelling wave problem 
+##
+###########################################################
 ## Step 1: Define the mesh 
-RmM = 4m / (m - 1) * (M / (4π))^((m - 1) / m)
-L = sqrt(RmM) * (D / (λ * (m - 1)) * (exp(λ * (m - 1) * final_time) - 1))^(1 / (2m))
-n = 500
-x₁ = LinRange(-L, L, n)
-x₂ = LinRange(L, L, n)
-x₃ = LinRange(L, -L, n)
-x₄ = LinRange(-L, -L, n)
-y₁ = LinRange(-L, -L, n)
-y₂ = LinRange(-L, L, n)
-y₃ = LinRange(L, L, n)
-y₄ = LinRange(L, -L, n)
-x = reduce(vcat, [x₁, x₂, x₃, x₄])
-y = reduce(vcat, [y₁, y₂, y₃, y₄])
-xy = [(x, y) for (x, y) in zip(x, y)]
-unique!(xy)
-x = getx.(xy)
-y = gety.(xy)
-r = 0.07
-T, adj, adj2v, DG, points, BN = generate_mesh(x, y, r; gmsh_path=GMSH_PATH)
+a, b, c, d, Nx, Ny = 0.0, 3.0, 0.0, 40.0, 60, 80
+T, adj, adj2v, DG, points, BN = triangulate_structured(a, b, c, d, Nx, Ny; return_boundary_types=true)
 mesh = FVMGeometry(T, adj, adj2v, DG, points, BN)
 
 ## Step 2: Define the boundary conditions 
-bc = ((x, y, t, u::T, p) where {T}) -> zero(T)
-types = :D
-BCs = BoundaryConditions(mesh, bc, types, BN)
+a₁ = ((x, y, t, u::T, p) where {T}) -> one(T)
+a₂ = ((x, y, t, u::T, p) where {T}) -> zero(T)
+a₃ = ((x, y, t, u::T, p) where {T}) -> zero(T)
+a₄ = ((x, y, t, u::T, p) where {T}) -> zero(T)
+bc_fncs = (a₁, a₂, a₃, a₄)
+types = (:D, :N, :D, :N)
+BCs = BoundaryConditions(mesh, bc_fncs, types, BN)
 
-## Step 3: Define the exact solution for comparison later 
-function porous_medium_exact_solution(x, y, t, m, M)
-    u_exact = zeros(length(x))
-    RmM = 4m / (m - 1) * (M / (4π))^((m - 1) / m)
-    for i in eachindex(x)
-        if x[i]^2 + y[i]^2 < RmM * t^(1 / m)
-            u_exact[i] = t^(-1 / m) * ((M / (4π))^((m - 1) / m) - (m - 1) / (4m) * (x[i]^2 + y[i]^2) * t^(-1 / m))^(1 / (m - 1))
-        else
-            u_exact[i] = 0.0
-        end
-    end
-    return u_exact
-end
-function porous_medium_linear_source_exact_solution(x, y, t, m, M, D, λ)
-    return exp(λ*t) * porous_medium_exact_solution(x, y, D/(λ*(m-1))*(exp(λ*(m-1)*t)-1), m, M)
-end
-
-## Step 4: Define the actual PDE  
-f = (x, y) -> M * 1 / (ε^2 * π) * exp(-1 / (ε^2) * (x^2 + y^2))
-diff_fnc = (x, y, t, u, p) -> p[1] * abs(u)^(p[2] - 1)
-reac_fnc = (x, y, t, u, p) -> p[1] * u
-diff_parameters = (D, m)
-react_parameter = λ
+## Step 3: Define the actual PDE  
+f = ((x::T, y::T) where {T}) -> zero(T)
+diff_fnc = (x, y, t, u, p) -> p * u
+reac_fnc = (x, y, t, u, p) -> p * u * (1 - u)
+D, λ = 0.9, 0.99
+diff_parameters = D
+reac_parameters = λ
+final_time = 50.0
 u₀ = [f(points[:, i]...) for i in axes(points, 2)]
-prob = FVMProblem(mesh, BCs; diffusion_function=diff_fnc,
-    diffusion_parameters=diff_parameters,
-    reaction_function=reac_fnc, reaction_parameters=react_parameter,
+prob = FVMProblem(mesh, BCs; diffusion_function=diff_fnc, reaction_function=reac_fnc,
+    diffusion_parameters=diff_parameters, reaction_parameters=reac_parameters,
     initial_condition=u₀, final_time)
 
-## Step 5: Solve
+## Step 4: Solve
 alg = TRBDF2(linsolve=KLUFactorization())
-sol = solve(prob, alg; saveat=2.5)
+sol = solve(prob, alg; saveat=0.5)
 
-## Step 6: Visualisation 
+## Step 5: Visualisation 
 pt_mat = Matrix(points')
 T_mat = [collect(T)[i][j] for i in 1:length(T), j in 1:3]
-fig = Figure(resolution=(2131.8438f0, 684.27f0), fontsize=38)
+fig = Figure(resolution=(3023.5881f0, 684.27f0), fontsize=38)
 ax = Axis(fig[1, 1], width=600, height=600)
-mesh!(ax, pt_mat, T_mat, color=sol.u[1], colorrange=(0.0, 0.5), colormap=:matter)
+mesh!(ax, pt_mat, T_mat, color=sol.u[1], colorrange=(0.0, 1.0), colormap=:matter)
 ax = Axis(fig[1, 2], width=600, height=600)
-mesh!(ax, pt_mat, T_mat, color=sol.u[3], colorrange=(0.0, 0.5), colormap=:matter)
+mesh!(ax, pt_mat, T_mat, color=sol.u[51], colorrange=(0.0, 1.0), colormap=:matter)
 ax = Axis(fig[1, 3], width=600, height=600)
-mesh!(ax, pt_mat, T_mat, color=sol.u[5], colorrange=(0.0, 0.5), colormap=:matter)
-SAVE_FIGURE && save("figures/porous_medium_linear_source_test.png", fig)
+mesh!(ax, pt_mat, T_mat, color=sol.u[101], colorrange=(0.0, 1.0), colormap=:matter)
 
-## Step 7: Compare the results
-all_errs = [Float64[] for _ in eachindex(sol)]
-u_exact = [porous_medium_linear_source_exact_solution(points[1, :], points[2, :], τ, m, M, D, λ) for τ in sol.t]
-u_fvm = reduce(hcat, sol.u)
-u_exact = reduce(hcat, u_exact)
-errs = reduce(hcat, [100abs.(u - û) / maximum(abs.(u)) for (u, û) in zip(eachcol(u_exact[:, 2:end]), eachcol(u_fvm[:, 2:end]))])
-@test all(<(0.26), mean.(eachcol(errs)))
-@test all(<(0.17), median.(eachcol(errs)))
-@test mean(errs) < 0.25
-@test median(errs) < 0.05
+## Step 6: Put the solutions into a matrix format and test the x-invariance by comparing each column to the middle
+u_mat = [reshape(u, (Nx, Ny)) for u in sol.u]
+all_errs = zeros(length(sol))
+err_cache = zeros((Nx - 1) * Ny)
+for i in eachindex(sol)
+    u = u_mat[i]
+    ctr = 1
+    for j in union(1:((Nx÷2)-1), ((Nx÷2)+1):Nx)
+        for k in 1:Ny
+            err_cache[ctr] = 100abs(u[j, k] .- u[Nx÷2, k])
+            ctr += 1
+        end
+    end
+    all_errs[i] = mean(err_cache)
+end
+@test all(all_errs .< 0.05)
 
-## Step 8: Visualise the comparison 
-fig = Figure(fontsize=42, resolution=(3586.6597f0, 1466.396f0))
-ax = Axis(fig[1, 1], width=600, height=600, title=L"(a):$ $ Exact solution, $t = %$(sol.t[1])$", titlealign=:left)
-mesh!(ax, pt_mat, T_mat, color=u_exact[:, 1], colorrange=(0, 0.05), colormap=:matter)
-ax = Axis(fig[1, 2], width=600, height=600, title=L"(b):$ $ Exact solution, $t = %$(sol.t[2])$", titlealign=:left)
-mesh!(ax, pt_mat, T_mat, color=u_exact[:, 2], colorrange=(0, 0.05), colormap=:matter)
-ax = Axis(fig[1, 3], width=600, height=600, title=L"(c):$ $ Exact solution, $t = %$(sol.t[3])$", titlealign=:left)
-mesh!(ax, pt_mat, T_mat, color=u_exact[:, 3], colorrange=(0, 0.05), colormap=:matter)
-ax = Axis(fig[1, 4], width=600, height=600, title=L"(d):$ $ Exact solution, $t = %$(sol.t[4])$", titlealign=:left)
-mesh!(ax, pt_mat, T_mat, color=u_exact[:, 4], colorrange=(0, 0.05), colormap=:matter)
-ax = Axis(fig[1, 5], width=600, height=600, title=L"(e):$ $ Exact solution, $t = %$(sol.t[5])$", titlealign=:left)
-mesh!(ax, pt_mat, T_mat, color=u_exact[:, 5], colorrange=(0, 0.05), colormap=:matter)
-ax = Axis(fig[2, 1], width=600, height=600, title=L"(f):$ $ Numerical solution, $t = %$(sol.t[1])$", titlealign=:left)
-mesh!(ax, pt_mat, T_mat, color=u_fvm[:, 1], colorrange=(0, 0.05), colormap=:matter)
-ax = Axis(fig[2, 2], width=600, height=600, title=L"(g):$ $ Numerical solution, $t = %$(sol.t[2])$", titlealign=:left)
-mesh!(ax, pt_mat, T_mat, color=u_fvm[:, 2], colorrange=(0, 0.05), colormap=:matter)
-ax = Axis(fig[2, 3], width=600, height=600, title=L"(h):$ $ Numerical solution, $t = %$(sol.t[3])$", titlealign=:left)
-mesh!(ax, pt_mat, T_mat, color=u_fvm[:, 3], colorrange=(0, 0.05), colormap=:matter)
-ax = Axis(fig[2, 4], width=600, height=600, title=L"(i):$ $ Numerical solution, $t = %$(sol.t[4])$", titlealign=:left)
-mesh!(ax, pt_mat, T_mat, color=u_fvm[:, 4], colorrange=(0, 0.05), colormap=:matter)
-ax = Axis(fig[2, 5], width=600, height=600, title=L"(j):$ $ Numerical solution, $t = %$(sol.t[5])$", titlealign=:left)
-mesh!(ax, pt_mat, T_mat, color=u_fvm[:, 5], colorrange=(0, 0.05), colormap=:matter)
-SAVE_FIGURE && save("figures/porous_medium_linear_source_test_error.png", fig)
+## Step 7: Now compare to the exact (travelling wave) solution 
+large_time_idx = findfirst(sol.t .== 10)
+c = sqrt(λ / (2D))
+cₘᵢₙ = sqrt(λ * D / 2)
+zᶜ = 0.0
+exact_solution = ((z::T) where {T}) -> ifelse(z ≤ zᶜ, 1 - exp(cₘᵢₙ * (z - zᶜ)), zero(T))
+err_cache = zeros(Nx * Ny)
+all_errs = zeros(length(sol) - large_time_idx + 1)
+for (i, t_idx) in pairs(large_time_idx:lastindex(sol))
+    u = u_mat[t_idx]
+    τ = sol.t[t_idx]
+    ctr = 1
+    for j in 1:Nx
+        for k in 1:Ny
+            y = c + (k - 1) * (d - c) / (Ny - 1)
+            z = y - c * τ
+            exact_wave = exact_solution(z)
+            err_cache[ctr] = abs(u[j, k] - exact_wave)
+            ctr += 1
+        end
+    end
+    all_errs[i] = mean(err_cache)
+end
+@test all(all_errs .< 0.1)
+
+## Step 8: Visualise the comparison with the travelling wave
+travelling_wave_values = zeros(Ny, length(sol) - large_time_idx + 1)
+z_vals = zeros(Ny, length(sol) - large_time_idx + 1)
+for (i, t_idx) in pairs(large_time_idx:lastindex(sol))
+    u = u_mat[t_idx]
+    τ = sol.t[t_idx]
+    for k in 1:Ny
+        y = c + (k - 1) * (d - c) / (Ny - 1)
+        z = y - c * τ
+        z_vals[k, i] = z
+        travelling_wave_values[k, i] = u[Nx÷2, k]
+    end
+end
+exact_z_vals = collect(LinRange(extrema(z_vals)..., 500))
+exact_travelling_wave_values = exact_solution.(exact_z_vals)
+
+ax = Axis(fig[1, 4], width=900, height=600)
+colors = cgrad(:matter, length(sol) - large_time_idx + 1; categorical=false)
+[lines!(ax, z_vals[:, i], travelling_wave_values[:, i], color=colors[i], linewidth=2) for i in 1:(length(sol)-large_time_idx+1)]
+lines!(ax, exact_z_vals, exact_travelling_wave_values, color=:red, linewidth=4, linestyle=:dash)
+SAVE_FIGURE && save("figures/travelling_wave_problem_test.png", fig)
