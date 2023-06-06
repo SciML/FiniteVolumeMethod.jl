e10a59142bc250cfbd9a5f6cdfd33bafa7422e1d
DanielVandH <danj.vandenheuvel@gmail.com>
2023-06-06 11:34:14 +1000

    Put examples into one main section

--- docs/make.jl
+++ docs/make.jl
@@ -16,18 +16,19 @@ makedocs(;
         "Home" => "index.md",
         "Interface" => "interface.md",
         "Docstrings" => "docstrings.md",
-        "List of Examples and Setup" => "example_list.md",
-        "Example I: Diffusion equation on a square plate" => "diffusion_equation.md",
-        "Example II: Diffusion equation in a wedge with mixed boundary conditions" => "diffusion_equation_on_a_wedge.md",
-        "Example III: Reaction-diffusion equation with a time-dependent Dirichlet boundary condition on a disk" => "reaction_diffusion.md",
-        "Example IV: Porous-medium equation" => "porous_medium.md",
-        "Example V: Porous-Fisher equation and travelling waves" => "porous_fisher.md",
-        "Example VI: Using the linear interpolants" => "interpolants.md",
-        "Example VII: Diffusion equation on an annulus" => "annulus.md",
-        "Example VIII: Laplace's equation" => "laplace.md",
-        "Example IX: Mean exit time problems" => "mean_exit_time.md",
-        "Mathematical Details" => "math.md"
-    ]
+        "Examples" => [
+            "List of Examples and Setup" => "example_list.md",
+            "Example I: Diffusion equation on a square plate" => "diffusion_equation.md",
+            "Example II: Diffusion equation in a wedge with mixed boundary conditions" => "diffusion_equation_on_a_wedge.md",
+            "Example III: Reaction-diffusion equation with a time-dependent Dirichlet boundary condition on a disk" => "reaction_diffusion.md",
+            "Example IV: Porous-medium equation" => "porous_medium.md",
+            "Example V: Porous-Fisher equation and travelling waves" => "porous_fisher.md",
+            "Example VI: Using the linear interpolants" => "interpolants.md",
+            "Example VII: Diffusion equation on an annulus" => "annulus.md",
+            "Example VIII: Laplace's equation" => "laplace.md",
+            "Example IX: Mean exit time problems" => "mean_exit_time.md",
+            "Mathematical Details" => "math.md"
+        ]]
 )
 
 deploydocs(;
