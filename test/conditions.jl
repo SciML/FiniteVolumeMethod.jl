27c300e58b32febf96983d65b2ed538d302ea21a
DanielVandH <danj.vandenheuvel@gmail.com>
2023-09-15 11:21:45 +1000

    Some improved coverage

--- test/conditions.jl
+++ test/conditions.jl
@@ -34,8 +34,13 @@ end
     mesh = FVMGeometry(tri)
     f, t, p, g, q, dirichlet_nodes, dudt_nodes = example_bc_ic_setup()
     BCs = BoundaryConditions(mesh, f, t; parameters=p)
+    @test sprint(show, MIME"text/plain"(), BCs) == "BoundaryConditions with $(length(tri.boundary_index_ranges)) boundary conditions with types $(BCs.condition_types)"
     ICs = InternalConditions(g; dirichlet_nodes, dudt_nodes, parameters=q)
+    @test sprint(show, MIME"text/plain"(), ICs) == "InternalConditions with $(length(ICs.dirichlet_nodes)) Dirichlet nodes and $(length(ICs.dudt_nodes)) Dudt nodes"
     conds = FVM.Conditions(mesh, BCs, ICs)
+    @test sprint(show, MIME"text/plain"(), conds) == "Conditions with\n   $(length(conds.neumann_edges)) Neumann edges\n   $(length(conds.constrained_edges)) Constrained edges\n   $(length(conds.dirichlet_nodes)) Dirichlet nodes\n   $(length(conds.dudt_nodes)) Dudt nodes"
+    @test FVM.get_f(conds, 1) == conds.functions[1]
+    @test FVM.get_f(conds, 2) == conds.functions[2]
     @test BCs.condition_types == t
     @test ICs.dirichlet_nodes == dirichlet_nodes
     @test ICs.dudt_nodes == dudt_nodes
@@ -53,4 +58,26 @@ end
     @test FVM.has_dirichlet_nodes(conds)
     empty!(conds.dirichlet_nodes)
     @test !FVM.has_dirichlet_nodes(conds)
+    @test FVM.has_neumann_edges(conds)
+    empty!(conds.neumann_edges)
+    @test !FVM.has_neumann_edges(conds)
+    @test FVM.get_constrained_edges(conds) == conds.constrained_edges
+end
+
+@testset "apply_dudt_conditions!" begin
+    tri = example_tri_rect()
+    mesh = FVMGeometry(tri)
+    f, t, p, g, q, dirichlet_nodes, dudt_nodes = example_bc_ic_setup(; nothing_dudt=true)
+    BCs = BoundaryConditions(mesh, f, t; parameters=p)
+    ICs = InternalConditions(g; dirichlet_nodes, dudt_nodes, parameters=q)
+    conds = FVM.Conditions(mesh, BCs, ICs)
+    b = zeros(num_points(tri))
+    FVM.apply_dudt_conditions!(b, mesh, conds)
+    for (i, fidx) in dudt_nodes
+        if i ∈ keys(conds.dirichlet_nodes)
+            @test b[i] == 0.0
+        else
+            @test b[i] == conds.functions[fidx](get_point(tri, i)..., nothing, nothing)
+        end
+    end
 end
