1514c82fd1dbde397e35d269f4003ad58983d151
Documenter.jl <documenter@juliadocs.github.io>
2026-09-12 06:33:11 +0000

    build based on 1566adc

--- dev/literate_tutorials/diffusion_equation_in_a_wedge_with_mixed_boundary_conditions.jl
+++ dev/literate_tutorials/diffusion_equation_in_a_wedge_with_mixed_boundary_conditions.jl
@@ -1,7 +1,7 @@
 using DisplayAs #hide
 tc = DisplayAs.withcontext(:displaysize => (15, 80), :limit => true); #hide
-# # Diffusion Equation in a Wedge with Mixed Boundary Conditions 
-# In this example, we consider a diffusion equation on a wedge 
+# # Diffusion Equation in a Wedge with Mixed Boundary Conditions
+# In this example, we consider a diffusion equation on a wedge
 # with angle $\alpha$ and mixed boundary conditions:
 # ```math
 # \begin{equation*}
@@ -16,21 +16,21 @@ tc = DisplayAs.withcontext(:displaysize => (15, 80), :limit => true); #hide
 # ```
 # where we take $f(r,\theta) = 1-r$ and $\alpha=\pi/4$.
 #
-# Note that the PDE is provided in polar form, but Cartesian coordinates 
-# are assumed for the operators in our code. The conversion is easy, noting 
+# Note that the PDE is provided in polar form, but Cartesian coordinates
+# are assumed for the operators in our code. The conversion is easy, noting
 # that the two Neumann conditions are just equations of the form $\grad u \vdot \vu n = 0$.
-# Moreover, although the right-hand side of the PDE is given as a Laplacian, 
+# Moreover, although the right-hand side of the PDE is given as a Laplacian,
 # recall that $\grad^2 = \div\grad$, so we can write the PDE as $\partial u/\partial t + \div \vb q = 0$,
 # where $\vb q = -\grad u$.
 #
-# Let us now setup the problem. To define the geometry, 
-# we need to be careful that the `Triangulation` recognises 
+# Let us now setup the problem. To define the geometry,
+# we need to be careful that the `Triangulation` recognises
 # that we need to split the boundary into three parts,
-# one part for each boundary condition. This is accomplished 
+# one part for each boundary condition. This is accomplished
 # by providing a single vector for each part of the boundary as follows
 # (and as described in DelaunayTriangulation.jl's documentation),
-# where we also `refine!` the mesh to get a better mesh. For the arc, 
-# we use the `CircularArc` so that the mesh knows that it is triangulating 
+# where we also `refine!` the mesh to get a better mesh. For the arc,
+# we use the `CircularArc` so that the mesh knows that it is triangulating
 # a certain arc in that area.
 using DelaunayTriangulation, FiniteVolumeMethod, ElasticArrays
 using ReferenceTests, Bessels, FastGaussQuadrature, Cubature #src
@@ -43,7 +43,7 @@ upper_edge = [3, 1]
 boundary_nodes = [bottom_edge, [arc], upper_edge]
 tri = triangulate(points; boundary_nodes)
 A = get_area(tri)
-refine!(tri; max_area=1e-4A)
+refine!(tri; max_area = 1.0e-4A)
 mesh = FVMGeometry(tri)
 
 # This is the mesh we've constructed.
@@ -54,61 +54,65 @@ fig
 # To confirm that the boundary is now in three parts, see:
 get_boundary_nodes(tri)
 
-# We now need to define the boundary conditions. For this, 
-# we need to provide `Tuple`s, where the `i`th element of the 
-# `Tuple`s refers to the `i`th part of the boundary. The boundary 
+# We now need to define the boundary conditions. For this,
+# we need to provide `Tuple`s, where the `i`th element of the
+# `Tuple`s refers to the `i`th part of the boundary. The boundary
 # conditions are thus:
 lower_bc = arc_bc = upper_bc = (x, y, t, u, p) -> zero(u)
 types = (Neumann, Dirichlet, Neumann)
 BCs = BoundaryConditions(mesh, (lower_bc, arc_bc, upper_bc), types)
 
-# Now we can define the PDE. We use the reaction-diffusion formulation, 
-# specifying the diffusion function as a constant. 
+# Now we can define the PDE. We use the reaction-diffusion formulation,
+# specifying the diffusion function as a constant.
 f = (x, y) -> 1 - sqrt(x^2 + y^2)
 D = (x, y, t, u, p) -> one(u)
 initial_condition = [f(x, y) for (x, y) in DelaunayTriangulation.each_point(tri)]
 final_time = 0.1
-prob = FVMProblem(mesh, BCs; diffusion_function=D, initial_condition, final_time)
+prob = FVMProblem(mesh, BCs; diffusion_function = D, initial_condition, final_time)
 
-# If you did want to use the flux formulation, you would need to provide 
+# If you did want to use the flux formulation, you would need to provide
 flux = (x, y, t, α, β, γ, p) -> (-α, -β)
 
 # which replaces `u` with `αx + βy + γ` so that we approximate $\grad u$ by $(\alpha,\beta)^{\mkern-1.5mu\mathsf{T}}$,
 # and the negative is needed since $\vb q = -\grad u$.
 
 # We now solve the problem. We provide the solver for this problem.
-# In my experience, I've found that `TRBDF2(linsolve=KLUFactorization())` typically 
+# In my experience, I've found that `TRBDF2(linsolve=KLUFactorization())` typically
 # has the best performance for these problems.
-using OrdinaryDiffEq, LinearSolve
-sol = solve(prob, TRBDF2(linsolve=KLUFactorization()), saveat=0.01, parallel=Val(false))
+using OrdinaryDiffEq, OrdinaryDiffEqSDIRK, LinearSolve
+sol = solve(prob, TRBDF2(linsolve = KLUFactorization()), saveat = 0.01, parallel = Val(false))
 ind = findall(DelaunayTriangulation.each_point_index(tri)) do i #hide
     !DelaunayTriangulation.has_vertex(tri, i) #hide
 end #hide
 using Test #hide
-@test sol[ind, :] ≈ reshape(repeat(initial_condition, length(sol)), :, length(sol))[ind, :] # make sure that missing vertices don't change #hide
+@test sol[ind, :] ≈ reshape(repeat(initial_condition, length(sol.u)), :, length(sol.u))[ind, :] # make sure that missing vertices don't change #hide
 sol |> tc #hide
 
 #-
 using CairoMakie
-fig = Figure(fontsize=38)
+fig = Figure(fontsize = 38)
 for (i, j) in zip(1:3, (1, 6, 11))
     local ax
-    ax = Axis(fig[1, i], width=600, height=600,
-        xlabel="x", ylabel="y",
-        title="t = $(sol.t[j])",
-        titlealign=:left)
-    tricontourf!(ax, tri, sol.u[j], levels=0:0.01:1, colormap=:matter)
+    ax = Axis(
+        fig[1, i], width = 600, height = 600,
+        xlabel = "x", ylabel = "y",
+        title = "t = $(sol.t[j])",
+        titlealign = :left
+    )
+    tricontourf!(ax, tri, sol.u[j], levels = 0:0.01:1, colormap = :matter)
     tightlimits!(ax)
 end
 resize_to_layout!(fig)
 fig
-@test_reference joinpath(@__DIR__, "../figures", "diffusion_equation_in_a_wedge_with_mixed_boundary_conditions.png") fig #src
+@test_reference joinpath(
+    @__DIR__, "../figures", "diffusion_equation_in_a_wedge_with_mixed_boundary_conditions.png"
+) fig #src
 
 function get_ζ_terms(M, N, α) #src
     ζ = zeros(M, N + 2) #src
-    for n in 0:(N+1) #src
+    for n in 0:(N + 1) #src
         order = n * π / α #src
-        @views ζ[:, n+1] .= approx_besselroots(order, M) #src
+        @views ζ[:, n + 1] .= approx_besselroots(order, M) #src
     end #src
     return ζ #src
 end #src
@@ -118,8 +122,10 @@ function get_sum_coefficients(M, N, α, ζ) #src
     for n in 0:N #src
         order = n * π / α #src
         for m in 1:M #src
-            integrand = rθ -> _f(rθ[2], rθ[1]) * besselj(order, ζ[m, n+1] * rθ[2]) * cos(order * rθ[1]) * rθ[2] #src
-            A[m, n+1] = 4.0 / (α * besselj(order + 1, ζ[m, n+1])^2) * hcubature(integrand, [0.0, 0.0], [α, 1.0]; abstol=1e-8)[1] #src
+            integrand = rθ -> _f(rθ[2], rθ[1]) * besselj(order, ζ[m, n + 1] * rθ[2]) *
+                cos(order * rθ[1]) * rθ[2] #src
+            A[m, n + 1] = 4.0 / (α * besselj(order + 1, ζ[m, n + 1])^2) *
+                hcubature(integrand, [0.0, 0.0], [α, 1.0]; abstol = 1.0e-8)[1] #src
         end #src
     end #src
     return A #src
@@ -136,20 +142,21 @@ function exact_solution(x, y, t, A, ζ, f, α) #src
     end #src
     for n in 1:N #src
         order = n * π / α #src
-        for m = 1:M #src
-            s += +A[m, n+1] * exp(-ζ[m, n+1]^2 * t) * besselj(order, ζ[m, n+1] * r) * cos(order * θ) #src
+        for m in 1:M #src
+            s += +A[m, n + 1] * exp(-ζ[m, n + 1]^2 * t) * besselj(order, ζ[m, n + 1] * r) *
+                cos(order * θ) #src
         end #src
     end #src
     return s #src
 end #src
 function compare_solutions(sol, tri, α, f) #src
     n = DelaunayTriangulation.num_points(tri) #src
-    x = zeros(n, length(sol)) #src
-    y = zeros(n, length(sol)) #src
-    u = zeros(n, length(sol)) #src
+    x = zeros(n, length(sol.u)) #src
+    y = zeros(n, length(sol.u)) #src
+    u = zeros(n, length(sol.u)) #src
     ζ = get_ζ_terms(20, 20, α) #src
     A = get_sum_coefficients(20, 20, α, ζ) #src
-    for i in eachindex(sol) #src
+    for i in eachindex(sol.u) #src
         for j in each_solid_vertex(tri) #src
             x[j, i], y[j, i] = get_point(tri, j) #src
             u[j, i] = exact_solution(x[j, i], y[j, i], sol.t[i], A, ζ, f, α) #src
@@ -158,14 +165,17 @@ function compare_solutions(sol, tri, α, f) #src
     return x, y, u #src
 end #src
 x, y, u = compare_solutions(sol, tri, α, f) #src
-fig = Figure(fontsize=64) #src
-for i in eachindex(sol) #src
+fig = Figure(fontsize = 64) #src
+for i in eachindex(sol.u) #src
     local ax #src
-    ax = Axis(fig[1, i], width=600, height=600) #src
-    tricontourf!(ax, tri, sol.u[i], levels=0:0.01:1, colormap=:matter) #src
-    ax = Axis(fig[2, i], width=600, height=600) #src
-    tricontourf!(ax, tri, u[:, i], levels=0:0.01:1, colormap=:matter) #src
+    ax = Axis(fig[1, i], width = 600, height = 600) #src
+    tricontourf!(ax, tri, sol.u[i], levels = 0:0.01:1, colormap = :matter) #src
+    ax = Axis(fig[2, i], width = 600, height = 600) #src
+    tricontourf!(ax, tri, u[:, i], levels = 0:0.01:1, colormap = :matter) #src
 end #src
 resize_to_layout!(fig) #src
 fig #src
-@test_reference joinpath(@__DIR__, "../figures", "diffusion_equation_in_a_wedge_with_mixed_boundary_conditions_exact_comparisons.png") fig #src
+@test_reference joinpath(
+    @__DIR__, "../figures",
+    "diffusion_equation_in_a_wedge_with_mixed_boundary_conditions_exact_comparisons.png"
+) fig #src
--- dev/literate_tutorials/diffusion_equation_on_a_square_plate.jl
+++ dev/literate_tutorials/diffusion_equation_on_a_square_plate.jl
@@ -22,7 +22,7 @@ using ReferenceTests #src
 using StatsBase #src
 a, b, c, d = 0.0, 2.0, 0.0, 2.0
 nx, ny = 50, 50
-tri = triangulate_rectangle(a, b, c, d, nx, ny, single_boundary=true)
+tri = triangulate_rectangle(a, b, c, d, nx, ny, single_boundary = true)
 mesh = FVMGeometry(tri)
 
 # This mesh is shown below.
@@ -34,40 +34,42 @@ fig
 bc = (x, y, t, u, p) -> zero(u)
 BCs = BoundaryConditions(mesh, bc, Dirichlet)
 
-# We can now define the actual PDE. We start by defining the initial condition and the diffusion function. 
+# We can now define the actual PDE. We start by defining the initial condition and the diffusion function.
 f = (x, y) -> y ≤ 1.0 ? 50.0 : 0.0
 initial_condition = [f(x, y) for (x, y) in DelaunayTriangulation.each_point(tri)]
 D = (x, y, t, u, p) -> 1 / 9
 
 # We can now define the problem:
 final_time = 0.5
-prob = FVMProblem(mesh, BCs; diffusion_function=D, initial_condition, final_time)
+prob = FVMProblem(mesh, BCs; diffusion_function = D, initial_condition, final_time)
 
 # Note that in `prob`, it is not a diffusion function that is used but instead it is a flux function:
 prob.flux_function
 
 # When providing `diffusion_function`, the flux is given by $\vb q(\vb x, t, \alpha,\beta,\gamma) = (-\alpha/9, -\beta/9)^{\mkern-1.5mu\mathsf{T}}$,
-# where $(\alpha, \beta, \gamma)$ defines the approximation to $u$ via $u(x, y) = \alpha x + \beta y + \gamma$ so that 
+# where $(\alpha, \beta, \gamma)$ defines the approximation to $u$ via $u(x, y) = \alpha x + \beta y + \gamma$ so that
 # $\grad u(\vb x, t) = (\alpha,\beta)^{\mkern-1.5mu\mathsf{T}}$.
 
-# To now solve the problem, we simply use `solve`. Note that, 
+# To now solve the problem, we simply use `solve`. Note that,
 # in the `solve` call below, multithreading is enabled by default.
-# (If you don't know what algorithm to consider, do `using DifferentialEquations` instead 
-# and simply call `solve(prob, saveat=0.05)` so that the algorithm is chosen automatically instead 
+# (If you don't know what algorithm to consider, do `using DifferentialEquations` instead
+# and simply call `solve(prob, saveat=0.05)` so that the algorithm is chosen automatically instead
 # of using `Tsit5()`.)
 using OrdinaryDiffEq
-sol = solve(prob, Tsit5(), saveat=0.05)
+sol = solve(prob, Tsit5(), saveat = 0.05)
 sol |> tc #hide
 
-# To visualise the solution, we can use `tricontourf!` from Makie.jl. 
-fig = Figure(fontsize=38)
+# To visualise the solution, we can use `tricontourf!` from Makie.jl.
+fig = Figure(fontsize = 38)
 for (i, j) in zip(1:3, (1, 6, 11))
     local ax
-    ax = Axis(fig[1, i], width=600, height=600,
-        xlabel="x", ylabel="y",
-        title="t = $(sol.t[j])",
-        titlealign=:left)
-    tricontourf!(ax, tri, sol.u[j], levels=0:5:50, colormap=:matter)
+    ax = Axis(
+        fig[1, i], width = 600, height = 600,
+        xlabel = "x", ylabel = "y",
+        title = "t = $(sol.t[j])",
+        titlealign = :left
+    )
+    tricontourf!(ax, tri, sol.u[j], levels = 0:5:50, colormap = :matter)
     tightlimits!(ax)
 end
 resize_to_layout!(fig)
@@ -80,7 +82,8 @@ function exact_solution(x, y, t) #src
         for m in 1:2:50 #src
             mterm = 2 / m * sin(m * π * x / 2) * exp(-π^2 * m^2 * t / 36) #src
             for n in 1:50 #src
-                nterm = (1 - cos(n * π / 2)) / n * sin(n * π * y / 2) * exp(-π^2 * n^2 * t / 36) #src
+                nterm = (1 - cos(n * π / 2)) / n * sin(n * π * y / 2) *
+                    exp(-π^2 * n^2 * t / 36) #src
                 s += mterm * nterm #src
             end #src
         end #src
@@ -91,10 +94,10 @@ function exact_solution(x, y, t) #src
 end #src
 function compare_solutions(sol, tri) #src
     n = DelaunayTriangulation.num_points(tri) #src
-    x = zeros(n, length(sol)) #src
-    y = zeros(n, length(sol)) #src
-    u = zeros(n, length(sol)) #src
-    for i in eachindex(sol) #src
+    x = zeros(n, length(sol.u)) #src
+    y = zeros(n, length(sol.u)) #src
+    u = zeros(n, length(sol.u)) #src
+    for i in eachindex(sol.u) #src
         for j in each_solid_vertex(tri) #src
             x[j, i], y[j, i] = get_point(tri, j) #src
             u[j, i] = exact_solution(x[j, i], y[j, i], sol.t[i]) #src
@@ -103,13 +106,13 @@ function compare_solutions(sol, tri) #src
     return x, y, u #src
 end #src
 x, y, u = compare_solutions(sol, tri) #src
-fig = Figure(fontsize=64) #src
-for i in eachindex(sol) #src
+fig = Figure(fontsize = 64) #src
+for i in eachindex(sol.u) #src
     local ax #src
-    ax = Axis(fig[1, i], width=600, height=600) #src
-    tricontourf!(ax, tri, sol.u[i], levels=0:5:50, colormap=:matter) #src
-    ax = Axis(fig[2, i], width=600, height=600) #src
-    tricontourf!(ax, tri, u[:, i], levels=0:5:50, colormap=:matter) #src
+    ax = Axis(fig[1, i], width = 600, height = 600) #src
+    tricontourf!(ax, tri, sol.u[i], levels = 0:5:50, colormap = :matter) #src
+    ax = Axis(fig[2, i], width = 600, height = 600) #src
+    tricontourf!(ax, tri, u[:, i], levels = 0:5:50, colormap = :matter) #src
 end #src
 resize_to_layout!(fig) #src
 fig #src
